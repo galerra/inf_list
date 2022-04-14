f91c9e7dc45eddd7c8f0dc1081f85c5ed852987a
galerra <91934759+galerra@users.noreply.github.com>
2022-04-14 10:24:07 +0300

    Update 20.py

--- 20.py
+++ 20.py
@@ -1,16 +1,15 @@
+#шебанг
 if __name__ == "__main__":
-    a = list()
+    lst = []
     i = 1
+    c = 0
     while True:
-        i = float(input())
+        i = float(input('Введите число:'))
         if i == 0:
             break
         else:
-            a.append(i)
-
-    count = 0
-    for i in a:
-        if i == max(a):
-            count += 1
-
-    print(count)
+            lst.append(i)
+    for j in lst:
+        if j == max(lst):
+            c += 1
+    print(c)
