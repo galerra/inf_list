7cb7a7176f605ef32cdc4954c364beff92beb597
galerra <91934759+galerra@users.noreply.github.com>
2022-04-14 10:30:58 +0300

    Update 24.py

--- 24.py
+++ 24.py
@@ -1,8 +1,7 @@
+#шебанг
 if __name__ == "__main__":
-    a = list(map(int, input().split()))
-
-    for i, e in enumerate(a):
-        mn = min(range(i, len(a)), key=a.__getitem__)
-        a[i], a[mn] = a[mn], e
-
-    print(a)
+    lst = list(map(int, input().split()))
+    for i, e in enumerate(lst):
+        mn = min(range(i, len(lst)), key=lst.__getitem__) #енумирайт
+        lst[i], lst[mn] = lst[mn], e
+    print(lst)
